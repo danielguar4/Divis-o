4431a34ff7d442848d97cb4cc7841854e8389625
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-04-12 22:11:22 -0300

    Teste Editado

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double numerador, denominador, resultado;
+
+Console.Write("Digite o numerador....:");
+numerador = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Digite o numerador....:");
+denominador = Convert.ToDouble(Console.ReadLine());
+
+if (denominador != 0)
+{
+    resultado =numerador / denominador;
+    Console.WriteLine($"{numerador} dividido por {denominador} é {resultado}");
+}
+else
+{
+    Console.WriteLine("Não é possivel dividir por zero");
+}
